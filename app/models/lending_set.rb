08227f318cf73face933cd617c8b60877ffabf82
mikoto2000 <mikoto2000@gmail.com>
2024-09-09 12:18:03 +0000

    貸出セット更新に応じて、在庫状態が変更されるように修正。

--- app/models/lending_set.rb
+++ app/models/lending_set.rb
@@ -6,6 +6,20 @@ class LendingSet < ApplicationRecord
     ["book_stocks", "customer", "lending", "lending_status"]
   end
 
+  after_save do
+    if self.lending_status.name === "貸出中"
+      self.book_stocks.each {|bs|
+          bs.book_stock_status_id = 2
+          bs.save
+      }
+    elsif self.lending_status.name === "返却済"
+      self.book_stocks.each {|bs|
+          bs.book_stock_status_id = 1
+          bs.save
+      }
+    end
+  end
+
   validates :book_stocks, presence: true
   validates :lend_start_date, presence: true, date: true
   validates :return_deadline_date, presence: true, date: { after: :lend_start_date }
